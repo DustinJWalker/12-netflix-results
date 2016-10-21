ae5775f497fa9c2592c31a87c933b1b0be6d99e2
Dustin Walker <dwtimp@gmail.com>
2016-10-21 11:54:12 -0500

    added view and fetch request

--- app/index.js
+++ app/index.js
@@ -1,1 +1,15 @@
 import 'whatwg-fetch';
+import UserView from './views/netflix-results';
+
+
+// getting api data
+const uv = new UserView(doucument.querySelector('.user'), {});
+// fetch request
+fetch('http://netflixroulette.net/api/api.php?title=Attack%20on%20titan')
+.then((result) => result.json())
+.then((data) => {
+  const person = data.results[0];
+  uv.model = person;
+  uv.render();
+  document.querySelector('.user__first-name').innerText = person.name.first;
+});
--- app/views/netflix-results.js
+++ app/views/netflix-results.js
@@ -1,1 +1,22 @@
-export default class NetflixResultsView
+export default class NetflixResultsView {
+  /**
+   * el: Element - the existing element represetning one user
+   * model: object or array - starting value for model data
+   */
+
+  constructor(el, model) {
+    this.el = el;
+    this.model = model;
+  }
+
+  render() {
+    console.log(this);
+    document.querySelector('.netflix__show-title').innerText = this.model.show - title;
+    document.querySelector('.netflix__category').innerText = this.model.category;
+    document.querySelector('.netflix__rating').innerText = this.model.rating;
+    document.querySelector('.netflix__poster').innerText = this.model.poster;
+    document.querySelector('.netflix__summary').innerText = this.model.summary;
+    document.querySelector('.netflix__show-cast').innerText = this.model.cast;
+  }
+
+}
