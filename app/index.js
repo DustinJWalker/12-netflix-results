6adec8397f304408b5c2bc66365bb203807a755b
Dustin Walker <dwtimp@gmail.com>
2016-10-30 10:28:57 -0500

    updated event listener

--- app/index.js
+++ app/index.js
@@ -5,11 +5,22 @@ import NetflixResultsView from './views/netflix-results';
 // getting api data
 const programmingEl = document.querySelector('.programming');
 const input = document.querySelector('.search__title');
+const result = document.querySelector('.search__result');
+const seachBtn = document.querySelector('.search-form__btn');
+
 const uv = new NetflixResultsView(programmingEl, {});
-fetch('http://netflixroulette.net/api/api.php?title=Attack%20on%20titan')
+
+function search(show_title) {
+  fetch(`http://netflixroulette.net/api/api.php?title=${show_title}`)
 .then((result) => result.json())
 .then((data) => {
   const programming = data;
   uv.model = programming;
   uv.render();
+});
+}
+
+searchForm.addEventListener('submit', (ev) => {
+  ev.preventDefault();
+  search(input.value);
 });
