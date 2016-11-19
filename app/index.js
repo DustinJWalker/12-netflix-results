60db5f5475bc56c0d175e229a24fde78c141241a
Dustin Walker <dwtimp@gmail.com>
2016-11-18 20:57:05 -0600

    fixed styling

--- app/index.js
+++ app/index.js
@@ -8,19 +8,24 @@ const input = document.querySelector('.search__title');
 const result = document.querySelector('.search__result');
 const seachBtn = document.querySelector('.search-form__btn');
 
+let apiUrl = 'http://netflixroulette.net/api/api.php';
 const uv = new NetflixResultsView(programmingEl, {});
+searchForm.addEventListener('submit', (ev) => {
+  ev.preventDefault();
+});
 
-function search(show_title) {
-  fetch(`http://netflixroulette.net/api/api.php?title=${show_title}`)
-.then((result) => result.json())
+fetch(`${apiUrl}?title=${input.value}`)
+.then((response) => response.json())
 .then((data) => {
   const programming = data;
   uv.model = programming;
   uv.render();
 });
-}
 
-searchForm.addEventListener('submit', (ev) => {
-  ev.preventDefault();
-  search(input.value);
-});
+fetch(`${apiUrl}?title=Attack%20on%20titan`)
+  .then((response) => response.json())
+  .then((data) => {
+    const program = data;
+    uv.model = program;
+    uv.render();
+  });
--- app/views/netflix-results.js
+++ app/views/netflix-results.js
@@ -10,12 +10,12 @@ export default class NetflixResultsView {
   }
 
   render() {
-    document.querySelector('.netflix__show-title').innerText = this.model.show_title;
-    document.querySelector('.netflix__category').innerText = this.model.category;
-    document.querySelector('.netflix__rating').innerText = this.model.rating;
-    document.querySelector('.netflix__poster').src = this.model.poster;
-    document.querySelector('.netflix__summary').innerText = this.model.summary;
-    document.querySelector('.netflix__show-cast').innerText = this.model.show_cast;
+    this.el.querySelector('.netflix__show-title').innerText = this.model.show_title;
+    this.el.querySelector('.netflix__category').innerText = this.model.category;
+    this.el.querySelector('.netflix__rating').innerText = this.model.rating;
+    this.el.querySelector('.netflix__poster').src = this.model.poster;
+    this.el.querySelector('.netflix__summary').innerText = this.model.summary;
+    this.el.querySelector('.netflix__show-cast').innerText = this.model.show_cast;
   }
 
 }
