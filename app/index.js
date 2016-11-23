0cdc70e891acf20a0f6d2b91b9979ac0fc3bd532
Dustin Walker <dwtimp@gmail.com>
2016-11-23 14:29:27 -0600

    finished

--- app/index.js
+++ app/index.js
@@ -4,24 +4,28 @@ import NetflixResultsView from './views/netflix-results';
 
 // getting api data
 const programmingEl = document.querySelector('.programming');
-const input = document.querySelector('.search__title');
-const result = document.querySelector('.search__result');
-const searchBtn = document.querySelector('.search-form__btn');
+const input = document.querySelector('.search-form__input');
+const form = document.querySelector('.search-form');
 
-const apiUrl = 'http://netflixroulette.net/api/api.php';
+// const apiUrl = 'http://netflixroulette.net/api/api.php';
 const uv = new NetflixResultsView(programmingEl, {});
 
-function searchProgramming(content) {
-  fetch('http://netflixroulette.net/api/api.php?title=Attack%20on%20titan')
-  .then((response) => response.json())
-  .then((data) => {
-    const programming = data;
-    uv.model = programming;
-    uv.render();
-  });
+function searchProgramming(show) {
+  fetch(`http://netflixroulette.net/api/api.php?title=${show}`)
+    .then((response) => response.json())
+    .then((data) => {
+      const programming = data;
+      uv.model = programming;
+      uv.render();
+    });
 }
 
+searchProgramming('Attack%20on%20titan');
 
-searchForm.addEventListener('submit', (ev) => {
+form.addEventListener('submit', (ev) => {
+  // Stop the form from reloading the page
   ev.preventDefault();
+
+  // Make fetch request
+  searchProgramming(input.value);
 });
